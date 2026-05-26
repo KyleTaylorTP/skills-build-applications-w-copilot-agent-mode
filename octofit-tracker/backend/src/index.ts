c8705a0a440f8a4a5f27b70fe95fd378ff2d7e98
KyleTaylorTP <kyle.taylor01@transpower.co.nz>
2026-05-26 09:30:39 +0000

    rename api.ts

--- octofit-tracker/backend/src/index.ts
+++ octofit-tracker/backend/src/index.ts
@@ -2,7 +2,7 @@ import express, { Express, Request, Response } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { connectDatabase } from './config/database';
-import { getApiUrl, getApiBaseUrl, isCodespaces } from './config/api';
+import { getApiUrl, getApiBaseUrl, isCodespaces } from './config/server';
 import usersRouter from './routes/users';
 import teamsRouter from './routes/teams';
 import activitiesRouter from './routes/activities';
